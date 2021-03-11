4d0ca767ba98a1732d4c96e78fded9e0f9ad3e5d
3neutronstar <3neutronstar@naver.com>
2021-03-12 00:15:52 +0900

    inheritance

--- Visualization/tensorboard.py
+++ Visualization/tensorboard.py
@@ -32,12 +32,59 @@ class Tensorboard():
 
 class Tensorboard_node(Tensorboard):
     def __init__(self, dataTensor, path, file_name, config):
-        super(Tensorboard_node, self).__init__()
+        super(Tensorboard_node, self).__init__(
+            dataTensor, path, file_name, config)
+        # dataTensor dim
+        # 0: time
+        # 1: w 1배
+        # 2: avg,norm
+
+    def time_write(self):
+        for t, data in enumerate(self.total_data):
+            tmp_data = data.detach().clone()
+
+            for l, num_w in enumerate(self.b_size_list):  # b인 이유: node관찰이므로
+                # weight
+                node_w = tmp_data[:num_w].detach().clone()
+                tmp_data = tmp_data[num_w:]
+                nodes_integrated_avg = dict()
+                nodes_integrated_norm = dict()
+                for n, node_info in enumerate(node_w):  # node 단위
+                    nodes_integrated_avg['{}l_{}n'.format(
+                        l, n)] = node_info[0]
+                    nodes_integrated_norm['{}l_{}n'.format(
+                        l, n)] = node_info[1]
+                    # self.timeWriter.add_scalar(
+                    #     'avg_grad/{}l_{}n'.format(l, n), node_info[0], t)  # 합
+                    # self.timeWriter.add_scalar(
+                    #     'norm_grad/{}l_{}n'.format(l, n), node_info[1], t)  # norm
+
+                self.timeWriter.add_scalars(
+                    '{}l/avg_of_grads'.format(l), nodes_integrated_avg, t)
+                self.timeWriter.add_scalars(
+                    '{}l/l2_norm_of_grads'.format(l), nodes_integrated_norm, t)
+
+            if t % 1000 == 0:
+                print('\r {} line complete'.format(t), end='')
+
+    def node_write(self):
+        sum_data = torch.sum(self.transposed_data, dim=1)
+        tmp_data = sum_data.detach().clone()
+        for l, num_w in enumerate(self.b_size_list):
+            node_w = tmp_data[:num_w].detach().clone()
+            tmp_data = tmp_data[num_w:]
+
+            for n, node_info in enumerate(node_w):  # node 단위
+                self.timeWriter.add_scalars(
+                    '{}l/avg_of_grads'.format(l), node_info[0], n)
+                self.timeWriter.add_scalars(
+                    '{}l/l2_norm_of_grads'.format(l), node_info[1], n)
 
 
 class Tensorboard_elem(Tensorboard):
     def __init__(self, dataTensor, path, file_name, config):
-        super(Tensorboard_elem, self).__init__()
+        super(Tensorboard_elem, self).__init__(
+            dataTensor, path, file_name, config)
 
     def time_write(self):
         # Gradient of node write in time
@@ -51,18 +98,18 @@ class Tensorboard_elem(Tensorboard):
                 # weight
                 tmp_w = tmp_data[:num_w]
                 tmp_data = tmp_data[num_w:]  # remove
+                nodes_integrated_avg = dict()
+                nodes_integrated_norm = dict()
                 # self.timeWriter.add_scalar('norm_grad/{}l'.format(l),tmp_w.norm(2),t)#norm in layer(all elem)
                 if self.NN_type_list[l] == 'cnn':
-                    nodes_integrated_sum = dict()
-                    nodes_integrated_norm = dict()
                     for n in range(self.NN_size_list[l+1]):  # node 단위
                         node_w = tmp_w[:(
                             self.kernel_size_list[l][0]*self.kernel_size_list[l][1])*self.NN_size_list[l]]
-                        nodes_integrated_sum['{}l_{}n'.format(
+                        nodes_integrated_avg['{}l_{}n'.format(
                             l, n)] = node_w.sum()
                         nodes_integrated_norm['{}l_{}n'.format(
                             l, n)] = node_w.norm(2)
-                        # self.timeWriter.add_scalar('sum_grad/{}l_{}n'.format(l,n),node_w.sum(),t)#합
+                        # self.timeWriter.add_scalar('avg_grad/{}l_{}n'.format(l,n),node_w.sum(),t)#합
                         # self.timeWriter.add_scalar('norm_grad/{}l_{}n'.format(l,n),node_w.norm(2),t)#norm
                         tmp_w = tmp_w[(
                             self.kernel_size_list[l][0]*self.kernel_size_list[l][1])*self.NN_size_list[l]:]  # 내용 제거
@@ -70,21 +117,21 @@ class Tensorboard_elem(Tensorboard):
                 elif self.NN_type_list[l] == 'fc':
                     for n in range(self.NN_size_list[l+1]):  # node 단위
                         node_w = tmp_w[:self.NN_size_list[l]]
-                        nodes_integrated_sum['{}l_{}n'.format(
+                        nodes_integrated_avg['{}l_{}n'.format(
                             l, n)] = node_w.sum()
                         nodes_integrated_norm['{}l_{}n'.format(
                             l, n)] = node_w.norm(2)
-                        # self.timeWriter.add_scalar('sum_grad/{}l_{}n'.format(l,n),node_w.sum(),t)#합
+                        # self.timeWriter.add_scalar('avg_grad/{}l_{}n'.format(l,n),node_w.sum(),t)#합
                         # self.timeWriter.add_scalar('norm_grad/{}l_{}n'.format(l,n),node_w.norm(2),t)#norm
                         tmp_w = tmp_w[self.NN_size_list[l]:]  # 내용제거
                 self.timeWriter.add_scalars(
-                    '{}l/sum_of_grads', nodes_integrated_sum, t)
+                    '{}l/avg_of_grads', nodes_integrated_avg, t)
                 self.timeWriter.add_scalars(
                     '{}l/l2_norm_of_grads', nodes_integrated_norm, t)
 
-            # bias
-            tmp_b = tmp_data[:num_b].detach().clone()
-            tmp_data = tmp_data[num_b:]  # remove
+                # bias
+                node_b = tmp_data[:num_b].detach().clone()
+                tmp_data = tmp_data[num_b:]  # remove
 
     def node_write(self):
         sum_time = torch.sum(self.transposed_data, dim=1)
@@ -100,15 +147,15 @@ class Tensorboard_elem(Tensorboard):
                     tmp_w = tmp_w[(
                         self.kernel_size_list[l][0]*self.kernel_size_list[l][1])*self.NN_size_list[l]:]  # 내용 제거
                     self.nodeWriter.add_scalar(
-                        '{}l/l2_norm_grad', node_w.norm(2), n)
+                        '{}l/l2_norm_grad'.format(l), node_w.norm(2), n)
 
             elif self.NN_type_list[l] == 'fc':
                 for n in range(self.NN_size_list[l+1]):  # node 단위
                     node_w = tmp_w[:self.NN_size_list[l]]
                     tmp_w = tmp_w[self.NN_size_list[l]:]  # 내용제거
                     self.nodeWriter.add_scalar(
-                        '{}l/l2_norm_grad', node_w.norm(2), n)
+                        '{}l/l2_norm_grad'.format(l), node_w.norm(2), n)
 
             # bias
-            tmp_b = tmp_data[:num_b].detach().clone()
+            node_b = tmp_data[:num_b].detach().clone()
             tmp_data = tmp_data[num_b:]  # remove
--- learning_rate.py
+++ learning_rate.py
@@ -1,6 +1,10 @@
-import torch 
+import torch
+import numpy as np
+
+
 class EarlyStopping:
     """주어진 patience 이후로 validation loss가 개선되지 않으면 학습을 조기 중지"""
+
     def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt'):
         """
         Args:
@@ -31,7 +35,8 @@ class EarlyStopping:
             self.save_checkpoint(val_loss, model)
         elif score < self.best_score + self.delta:
             self.counter += 1
-            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            print(
+                f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
@@ -42,6 +47,7 @@ class EarlyStopping:
     def save_checkpoint(self, val_loss, model):
         '''validation loss가 감소하면 모델을 저장한다.'''
         if self.verbose:
-            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+            print(
+                f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
         torch.save(model.state_dict(), self.path)
         self.val_loss_min = val_loss
