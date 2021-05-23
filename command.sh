c504490e0298410e5918381a540566001377b04c
3neutronstar <minsoo.kang0918@gmail.com>
2021-05-23 17:13:57 +0900

    modify shell scripts

--- command.sh
+++ command.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo 'train lenet5'
-conda activate bench
-MODE=$(train_lbl, baseline_moo)
-SET=$(0.1,0.2,0.3,0.4)
-SPARSE_VALUE=$(1,2,3)
+MODE=$(train_lbl baseline_moo)
+SET=$(0.1 0.2 0.3 0.4)
+SPARSE_VALUE=$(1 2 3)
 for VALUE in $SET; 
 do
     for SPARSE_VALUE in $SPARSE_SET
